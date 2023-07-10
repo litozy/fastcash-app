abfedeb0a42989cb03b7991e1f3227a05056f00d
rendyf3r <rendyf3r@gmail.com>
2023-07-10 19:15:06 +0700

    [FIX] fixing query for user in user_repo

--- utils/constant.go
+++ utils/constant.go
@@ -2,6 +2,10 @@ package utils
 
 //untuk naruh query
 
+//QUERY USER REPOSITORY
 const (
+	SELECT_ALL_USER = "SELECT id, user_name, is_active FROM user_credential"
+	INSERT_USER = "INSERT INTO user_credential(id, user_name, password, is_active) VALUES($1, $2, $3, $4)"
+	SELECT_USER_BY_NAME = "SELECT id, user_name, password, is_active FROM user_credential WHERE user_name = $1"
 	
 )
